3aa886b31a23e592d26a7bb805cd3fd397251eb2
Muhammad Ali <m.aleydev@gmail.com>
2021-02-03 08:28:14 +0500

    file updated

--- Timer/script.js
+++ Timer/script.js
@@ -7,14 +7,23 @@ function timer(){
     timeR.innerHTML = timerr;
 }
 function start(){
+    var start = document.getElementById("start");
+    start.disabled = true;
     interval = setInterval(timer,10);
 }
  
 function pause(){
+    var pause = document.getElementById("pause");
+    pause.disabled = true;
    clearInterval(interval);
 }
 
 function reset(){
+    alert(Math.round(timerr*1000/100000)+" seconds");
+    var start = document.getElementById("start");
+    start.disabled = false;
+    var pause = document.getElementById("pause");
+    pause.disabled = false;
     timerr = 0;
     timeR.innerHTML = timerr;
     pause();
